f54be08b6b83f54d33e5aa63c7ffe144ca5883e8
gayananu21 <gayananuradhad@gmail.com>
2023-01-10 11:12:01 +0530

    Complete tutorial 8 container

--- lib/main.dart
+++ lib/main.dart
@@ -21,44 +21,20 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Home"),
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter tutorial 08"),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 100.0, bottom: 100.0),
+        height: 400.0,
+        width: double.infinity,
+        color: Colors.yellow,
+        child: Text(
+          "Flutter tutorial 08",
+          style: TextStyle(
+            fontSize: 20.0,
           ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.more_vert),
-              onPressed: () {},
-            ),
-          ],
-          flexibleSpace: Image.asset(
-            "assests/blue.jpeg",
-            fit: BoxFit.cover,
-          ),
-          bottom: const TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.directions_car)),
-              Tab(icon: Icon(Icons.directions_transit)),
-              Tab(icon: Icon(Icons.directions_bike)),
-            ],
-          ),
-          elevation: 2.0,
-        ),
-        body: const TabBarView(
-          children: [
-            Icon(Icons.directions_car),
-            Icon(Icons.directions_transit),
-            Icon(Icons.directions_bike),
-          ],
         ),
       ),
     );
